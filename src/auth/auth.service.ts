cc59fb75bc082498842707050247552f5ec012e4
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-08-05 18:28:51 +0530

    error fixed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersRepository } from './users.repository';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectRepository(User)
+    @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
   ) {}
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.usersRepository.createUser(authCredentialsDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,12 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
-@EntityRepository()
+@EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  //
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
 }

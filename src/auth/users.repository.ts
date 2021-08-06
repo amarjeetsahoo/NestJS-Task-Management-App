7f976eca1e3b7e5999b9c41e1e93cd8a0b2fc7ec
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-08-06 18:42:49 +0530

    added:password encryption

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,12 +5,18 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    //hashing
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {

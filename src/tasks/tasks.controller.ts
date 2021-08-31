d36dbeaed56ecd5246446a3f5c44c79eb8c39510
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-08-31 11:50:33 +0530

    Gaurding task Route

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,15 +7,17 @@ import {
   Patch,
   Post,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { TaskStatus } from './task-status.enum';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 

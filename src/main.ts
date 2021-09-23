255d7ceec89b8b4854b1220028e7e35378561cb5
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2021-09-23 20:51:50 +0530

    Initial Deployment

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
     app.useGlobalInterceptors(new TransformInterceptor());
-    const PORT = 3000;
+    const PORT = process.env.PORT;
     await app.listen(PORT);
     logger.log(`Application is running on ${PORT}`);
 }
